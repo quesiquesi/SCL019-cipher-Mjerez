83e953005dc5a6d1e6cb6cf50b6ebcd6b9cee6bc
quesiquesi <mjesusjerezj@gmail.com>
2021-12-01 09:40:16 -0300

    esqueleto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
+
+
+
+function msg(){
+alert("¡Listo! tu mensaje está cifrado");
+}
+var valor = document.getElementById("mensaje").value;
+
+<textarea id="mensaje"></textarea>
+var valor = document.getElementById("mensaje").value;
+
+
 const cipher = {
   // ...
-};
+}
 
 export default cipher;
