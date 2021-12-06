432e56e52be055113d369a65c8f7792d8f7ddf34
amandamheredia <amanda.mnzh@gmail.com>
2021-12-05 21:46:47 -0300

    rebase

--- src/cipher.js
+++ src/cipher.js
@@ -3,6 +3,7 @@ const cifrador = document.getElementById('cifrador');
 const resultado = document.getElementById('resultado');
 const rango = document.getElementById('rango');
 
+<<<<<<< HEAD
 const shifMessage = ()=> {
 const wordArray = [...inputOriginal.value.toUpperCase()]
 }
@@ -13,6 +14,9 @@ const submit = e => {
     shifMessage()
 }
 cifrador.onsubmit = submit;
+=======
+
+>>>>>>> e3dc23e (feature(cifrador): no sé qué hice)
 
 const cipher = {
     // ...
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+function msg(){
+    alert("¡Listo! tu mensaje está cifrado");
+    }
+    var valor = document.getElementById("mensaje").value;
+    
+    <textarea id="mensaje"></textarea>
+    var valor = document.getElementById("mensaje").value;
+    
+
 import cipher from './cipher.js';
 
 console.log(cipher);
