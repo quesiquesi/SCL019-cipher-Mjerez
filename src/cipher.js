103ec1b1c0cf1a68f0ac590007acfa393b3a7130
amandamheredia <amanda.mnzh@gmail.com>
2021-12-06 12:28:07 -0300

    feature(cifrador): se añadieron funciones js

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,32 @@
-const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const inputOriginal = document.getElementById('input-original');
 const cifrador = document.getElementById('cifrador');
 const resultado = document.getElementById('resultado');
 const rango = document.getElementById('rango');
 
 const shifMessage = ()=> {
-    const wordArray = [...inputOriginal.value.toUpperCase()]
+    const wordArray = [...inputOriginal.value.toUpperCase()];
     printChar(0, wordArray);
 }
 
 const printChar = (currentLetterIndex, wordArray) => {
-    if(wordArray.lenght === currentLetterIndex) return;
+    if(wordArray.length === currentLetterIndex) return;
     inputOriginal.value = inputOriginal.value.substring(1)
     const spanChar = document.createElement("span");
     resultado.appendChild(spanChar);
     const charsinCodificar = wordArray[currentLetterIndex];
-    spanChar.innerHTML = alfabeto.includes(charsinCodificar); 
-     alfabeto[(alfabeto.indexOf(charsinCodificar) +parseInt(rango.value)) %alfabeto.length] ;
+    spanChar.innerHTML = alfabeto.includes(charsinCodificar) ? 
+     alfabeto[(alfabeto.indexOf(charsinCodificar) +parseInt(rango.value)) %alfabeto.length] :
      charsinCodificar
     printChar(currentLetterIndex +1, wordArray);
-
-
 }
 
 const submit = e => {
     e.preventDefault();
     resultado.innerHTML = '';
-    shifMessage()
+    shifMessage();
 }
+
 cifrador.onsubmit = submit;
 
 const cipher = {
@@ -35,4 +34,3 @@ const cipher = {
   }
   
   export default cipher
- 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-function msg(){
-    alert("¡Listo! tu mensaje está cifrado");
-    }
-    var valor = document.getElementById("mensaje").value;
-    
-    <textarea id="mensaje"></textarea>
-    var valor = document.getElementById("mensaje").value;
-    
-
 import cipher from './cipher.js';
 
 console.log(cipher);
