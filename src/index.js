9c901db6e711514f053555ea24d1ef5bb8f9d3f5
M Jerez <mjesusjerezj@gmail.com>
2021-12-13 09:08:56 -0300

    feature(cifrador): cambios en css

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 function myFunction() {
-    document.getElementById("demo").innerHTML = "¡Listo! tu mensaje está cifrado y seguro"
-  }
+    document.getElementById("demo").innerHTML = "¡Listo! tu mensaje está cifrado y seguro"}
