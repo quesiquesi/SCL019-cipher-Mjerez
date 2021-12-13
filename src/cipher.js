49cafa4dcac663eb3e0f8281148332c25154bc64
M Jerez <mjesusjerezj@gmail.com>
2021-12-13 10:16:06 -0300

    feature(cifrador): transparencia en el background color del flexbox

--- src/cipher.js
+++ src/cipher.js
@@ -34,3 +34,17 @@ function cifrar(texto, desplazamiento){
     }
     return resultado;
 }
+function cifrar2 (texto, desplazamiento){
+    if (!texto) 
+        return "";
+const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+desplazamiento = (desplazamiento % 26 + 26) %26;
+return texto.replace (/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26]);
+}
+function descifrar (texto, desplazamiento){
+    if (!texto) 
+        return "";
+const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+desplazamiento = (desplazamiento % 26 - 26) %26;
+return texto.replace (/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento)%26]);
+}
