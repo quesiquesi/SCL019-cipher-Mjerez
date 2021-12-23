5ce0e1d3767612cbd879f06dd84033c4aa0c151f
amandamheredia <amanda.mnzh@gmail.com>
2021-12-23 12:10:05 -0300

    feature(popup): boton abrir y cerrar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-var popup = document.getElementById('popup'),
-overlay = document.getElementById('overlay'),
-btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 
- btnCerrarPopup.addEventListener('click', function (e){
-     e.preventDefault();
-    overlay.classList.remove('active');
-    popup.classList.remove('active');
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
 
- });
+btnCerrarPopup.addEventListener('click', function(){
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
